a8a6845a03bff44373773c6c513be32dabba60e8
p-kolacz <piotr@kolacz.org>
2020-06-01 03:17:47 +0200

    (cat "$HOME/.dotfiles/git/messages" | shuf -n 1)

--- fish/.config/fish/conf.d/git.fish
+++ fish/.config/fish/conf.d/git.fish
@@ -2,5 +2,10 @@ if status is-interactive
 	abbr -ag g git status
 	abbr -ag gc "git add . && git commit -m"
 	abbr -ag gp git push
-	alias gpush="git add . && git commit -m '(cat "$HOME/.dotfiles/git/messages" | shuf -n 1)' && git push" 
 end
+
+function gpush
+	git add . && git commit -m '(cat "$HOME/.dotfiles/git/messages" | shuf -n 1)' 
+	# && git push 
+end
+

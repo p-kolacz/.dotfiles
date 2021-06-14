0df0ebdf378a63376168d71e4de1d1ec9e1466d0
p-kolacz <piotr@kolacz.org>
2021-06-14 21:04:36 +0200

    Fuckup.

--- bin/ytdl-audio.sh
+++ bin/ytdl-audio.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 $1
-youtube-dl -f bestaudio --extract-audio --add-metadata $1
+youtube-dl --extract-audio --audio-format mp3 --add-metadata $1
+# youtube-dl -f bestaudio --extract-audio --add-metadata $1

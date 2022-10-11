507e409e98e02d9bf97a86d28b8103dbc4a955b2
p-kolacz <piotr@kolacz.org>
2022-10-11 17:01:14 +0200

    Rats

---
+++ share/themes/eighties.light.sh
@@ -0,0 +1,22 @@
+export THEME_FG=#515151
+export THEME_BG=#f2f0ec
+export THEME_BLACK=#2d2d2d
+export THEME_BLACK2=#747369
+export THEME_RED=#f2777a
+export THEME_RED2=#f2777a
+export THEME_GREEN=#99cc99
+export THEME_GREEN2=#99cc99
+export THEME_YELLOW=#ffcc66
+export THEME_YELLOW2=#ffcc66
+export THEME_BLUE=#6699cc
+export THEME_BLUE2=#6699cc
+export THEME_MAGENTA=#cc99cc
+export THEME_MAGENTA2=#cc99cc
+export THEME_CYAN=#66cccc
+export THEME_CYAN2=#66cccc
+export THEME_WHITE=#d3d0c8
+export THEME_WHITE2=#f2f0ec
+
+export THEME_ACCENT="$THEME_GREEN"
+export THEME_VARIANT=light
+
---
+++ share/themes/rose-pine.sh
@@ -0,0 +1,22 @@
+export THEME_BG=#faf4ed
+export THEME_FG=#575279
+export THEME_BLACK=#575279
+export THEME_RED=#b4637a
+export THEME_GREEN=#286983
+export THEME_YELLOW=#ea9d34
+export THEME_BLUE=#cecacd
+export THEME_MAGENTA=#907aa9
+export THEME_CYAN=#56949f
+export THEME_WHITE=#f2e9e1
+export THEME_BLACK2=#797593
+export THEME_RED2=#eb6f92
+export THEME_GREEN2=#3e8fb0
+export THEME_YELLOW2=#f6c177
+export THEME_BLUE2=#dfdad9
+export THEME_MAGENTA2=#c4a7e7
+export THEME_CYAN2=#9ccfd8
+export THEME_WHITE2=#fffaf3
+
+export THEME_ACCENT="$THEME_RED"
+export THEME_VARIANT=light
+

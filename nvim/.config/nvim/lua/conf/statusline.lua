8dd888caff6737877238ca4c5825815c2f123301
p-kolacz <piotr@kolacz.org>
2022-10-14 17:02:59 +0200

    Revert "just testing, remember to revert"

--- nvim/.config/nvim/lua/conf/statusline.lua
+++ nvim/.config/nvim/lua/conf/statusline.lua
@@ -2,12 +2,34 @@ Plugin "https://github.com/nvim-lualine/lualine.nvim"
 
 -- Plugin "https://github.com/WhoIsSethDaniel/lualine-lsp-progress.nvim"
 
+local section_separators= { '',
+	{ left = '', right = '' },
+	{ left = '', right = '' },
+	{ left = '', right = '' },
+	{ left = '', right = ' ' },
+	{ left = '', right = '' },
+	{ left = '', right = '' },
+}
+local component_separators= { '',
+	{ left = '', right = '' },
+	{ left = '', right = '' },
+	{ left = '', right = '' },
+	{ left = '', right = '' },
+	{ left = '', right = '' },
+	{ left = '', right = '' },
+}
+
+math.randomseed(os.time())
+local random = math.random(#section_separators)
+
 require "lualine".setup {
 	options = {
 		theme = "auto",
 		-- theme = "papercolor_light",
 		-- theme = "solarized",
 		-- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md#available-themes
+		component_separators = component_separators[random],
+		section_separators = section_separators[random],
 		globalstatus = true,
 	},
 	sections = {
@@ -15,10 +37,20 @@ require "lualine".setup {
 		lualine_x = {'encoding', 'fileformat', 'filetype'},
 	},
 	winbar = {
-		lualine_b = {'filename',},
+		lualine_b = {
+			{
+				'filename',
+				path = 1,
+			},
+		},
 	},
 	inactive_winbar= {
-		lualine_b = {'filename',},
+		lualine_b = {
+			{
+				'filename',
+				path = 1,
+			},
+		},
 	},
 	tabline = {
 		lualine_z = {"tabs"},

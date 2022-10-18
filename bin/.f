b61ae04a32f3e00cb2c1850aea755261518f81b7
p-kolacz <piotr@kolacz.org>
2022-10-18 09:14:22 +0200

    .f

--- bin/.f
+++ bin/.f
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "Hello from .f!"
+_git() {
+	git --git-dir="$HOME/.config/dotfiles" --work-tree="$HOME" "$@"
+}
 
+if [[ $(typeset -f "$1") ]]; then
+	$1
+else
+	_git "$@"
+fi
+
+
+# config config --local status.showUntrackedFiles no
+# https://www.atlassian.com/git/tutorials/dotfiles

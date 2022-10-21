cdb4e0195c2e4adb5c54cb2e785f9de51cef5a96
p-kolacz <piotr@kolacz.org>
2022-10-21 17:43:01 +0200

    I would rather be playing SC2.

--- share/themes/rose-pine.sh
+++ share/themes/rose-pine.sh
@@ -4,21 +4,37 @@ export THEME_VARIANT=light
 export THEME_BG=#faf4ed
 export THEME_FG=#575279
 export THEME_BLACK=#575279
-export THEME_RED=#b4637a
-export THEME_GREEN=#286983
-export THEME_YELLOW=#ea9d34
-export THEME_BLUE=#cecacd
-export THEME_MAGENTA=#907aa9
-export THEME_CYAN=#56949f
-export THEME_WHITE=#f2e9e1
 export THEME_BLACK2=#797593
+export THEME_RED=#b4637a
 export THEME_RED2=#eb6f92
-export THEME_GREEN2=#3e8fb0
+export THEME_GREEN=#56949f
+export THEME_GREEN2=#9ccfd8
+export THEME_YELLOW=#ea9d34
 export THEME_YELLOW2=#f6c177
-export THEME_BLUE2=#dfdad9
-export THEME_MAGENTA2=#c4a7e7
-export THEME_CYAN2=#9ccfd8
+export THEME_BLUE=#31748f
+export THEME_BLUE2=#3e8fb0 
+export THEME_MAGENTA=#d7827e
+export THEME_MAGENTA2=#ea9a97
+export THEME_CYAN=#907aa9
+export THEME_CYAN2=#c4a7e7
+export THEME_WHITE=#f2e9e1
 export THEME_WHITE2=#fffaf3
 
 export THEME_ACCENT="$THEME_RED"
 
+                 # Base    Moon    Dawn
+# Base           #191724 #232136 #faf4ed
+# Surface        #1f1d2e #2a273f #fffaf3
+# Overlay        #26233a #393552 #f2e9e1
+# Muted          #6e6a86 #6e6a86 #9893a5
+# Subtle         #908caa #908caa #797593
+# Text           #e0def4 #e0def4 #575279
+# Love           #eb6f92 #eb6f92 #b4637a
+# Gold           #f6c177 #f6c177 #ea9d34
+# Rose           #ebbcba #ea9a97 #d7827e
+# Pine           #31748f #3e8fb0 #286983
+# Foam           #9ccfd8 #9ccfd8 #56949f
+# Iris           #c4a7e7 #c4a7e7 #907aa9
+# Highlight Low  #21202e #2a283e #f4ede8
+# Highlight Med  #403d52 #44415a #dfdad9
+# Highlight High #524f67 #56526e #cecacd

ff18a27a46222a3cb0e893e0812c67544728f981
p-kolacz <piotr@kolacz.org>
2022-10-25 17:09:00 +0200

    Small is a real HTML tag, who knew.

--- nvim/.config/nvim/lua/tool/gitsigns.lua
+++ nvim/.config/nvim/lua/tool/gitsigns.lua
@@ -2,27 +2,8 @@
 
 Plugin 'https://github.com/lewis6991/gitsigns.nvim'
 
-require('gitsigns').setup {
-	keymaps = {
-		-- Default keymap options
-		noremap = true,
-		buffer = true,
-
-		-- ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
-		-- ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
-
-		-- ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-		-- ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-		-- ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-		-- ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-		-- ['n <leader>gp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-		-- ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-
-		-- Text objects
-		-- ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-		-- ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
-	},
-}
+require('gitsigns').setup()
 
 nnoremap('<leader>gp', '<cmd>lua require"gitsigns".preview_hunk()<CR>', 'preview chunk')
+nnoremap("<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", "line blame")
 
